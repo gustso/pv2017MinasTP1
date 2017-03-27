062d732b5f9a28858f6a0cb381de0b8680b49c4a
GustsoNT <GustsoNT@192.168.1.133>
2017-03-27 13:41:31 -0300

    prueba

---
+++ src/ejemplo/HolaMundo.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo;
+
+/**
+ *
+ * @author gustso
+ */
+public class HolaMundo {
+    public static void main(String[] args){
+        String nombreMateria2 = "PV ";
+        saludarAlPublico("Juan Manuel \n ",nombreMateria2);
+        
+    }
+    
+    public static void saludarAlPublico(String nombreDeQuienSaluda,String nombreMateria){
+        System.out.println("Hola PV2015, saluda: " +nombreDeQuienSaluda + nombreMateria);
+        
+        //System.out.println(nombreDeQuienSaluda);
+    }
+    
+}
